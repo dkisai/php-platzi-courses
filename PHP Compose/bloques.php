b422900c10abeb4c28f63b2cded216b1064f976d
dKisai <diego.kisai@live.com>
2022-06-06 20:11:30 -0500

    bloques

--- PHP Compose/bloques.php
+++ PHP Compose/bloques.php
@@ -1,1 +1,46 @@
-<$php
+<?php
+
+if ('condition_1') {
+    # code...
+} elseif ('condition_2') {
+    # code...
+} else{
+    # code...
+}
+
+switch ($login) {
+    case true:
+        # code...
+        break;
+    case false:
+        #code...
+        break;
+    default:
+        # code...
+        break;
+}
+
+foreach ($variable as $key => $value) {
+    # code...
+}
+
+foreach ($datos as $row) {
+    # code...
+}
+
+
+$a = 2;
+while ($a <= 10) {
+    # code...
+    $a ++;
+}
+
+do {
+    # code...
+} while ($b <= 10);
+
+for ($i=0; $i < 5; $i++) { 
+    # code...
+}
+
+?>
